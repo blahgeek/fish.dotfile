bc2721c52f8aa9e3b834833553603ebbbecebf36
BlahGeek <blahgeek@gmail.com>
2013-12-28 18:26:22 +0800

    add virtualfish

--- config.fish
+++ config.fish
@@ -1,19 +1,25 @@
 . ~/.config/fish/autojump.fish
+. ~/.config/fish/virtualfish/virtual.fish
+. ~/.config/fish/virtualfish/auto_activation.fish
+. ~/.config/fish/virtualfish/global_requirements.fish
 
 alias vi="vim"
 alias du="du -h"
 alias df="df -h"
 alias x="dtrx -r -n"
 alias net_class="~/Documents/projects/net_class/net_class.py --path ~/Documents/netclass --size_limit 10000000"
-alias oclip="xsel -b"
-alias 2clip="xsel -ib"
+alias mypasswd="python2 /Users/BlahGeek/Documents/projects/mypasswd/mypasswd.py"
 alias pandoc="pandoc --latex-engine=xelatex -V mainfont=SimSun"
-alias mplayer-pitch="mplayer -af scaletempo=speed=pitch"
 alias mtr="mtr -t"
 alias ll="ls -alh"
 
 set -x EDITOR vim
 set -x PATH ~/.local/bin /usr/local/bin /usr/local/sbin $PATH
+
+set -x PIP_USE_WHEEL "true"
+set -x PIP_WHEEL_DIR "$HOME/.pip/wheels"
+set -x PIP_FIND_LINKS "$HOME/.pip/wheels"
+set -x PIP_DOWNLOAD_CACHE "$HOME/.pip/cache"
 
 function mkcd
     mkdir $argv; and cd $argv
--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -72,6 +72,10 @@ function fish_prompt --description 'Write out the prompt'
 	if not set -q __fish_prompt_host
 		set -g __fish_prompt_host (set_color cyan)
 	end
+	
+	if set -q VIRTUAL_ENV
+		echo -n -s (set_color -b blue white) "(" (basename "$VIRTUAL_ENV") ")" (set_color normal) " "
+	end
 
 	echo -n -s "$__fish_prompt_user" "$__fish_prompt_host" "$__fish_prompt_hostname" "$__fish_prompt_normal" ' ' "$__fish_prompt_cwd" (prompt_pwd) (__fish_git_prompt) "$__fish_prompt_normal" "$prompt_status" "$delim" ' '
 end
