69fb1f2709bf116adeb7475e184d08a268441082
BlahGeek <blahgeek@gmail.com>
2014-11-26 10:52:47 +0800

    revert autojump.fish

--- autojump.fish
+++ autojump.fish
@@ -1,5 +1,3 @@
-set -x AUTOJUMP_SOURCED 1
-
 # set user installation path
 if test -d ~/.autojump
     set -x PATH ~/.autojump/bin $PATH
@@ -10,20 +8,10 @@ end
 complete -x -c j -a '(autojump --complete (commandline -t))'
 
 
-# set error file location
-if (uname) == "Darwin"
-    set -x AUTOJUMP_ERROR_PATH ~/Library/autojump/errors.log
-else if test -d $XDG_DATA_HOME
-    set -x AUTOJUMP_ERROR_PATH $XDG_DATA_HOME/autojump/errors.log
-else
-    set -x AUTOJUMP_ERROR_PATH ~/.local/share/autojump/errors.log
-end
-
-
 # change pwd hook
 function __aj_add --on-variable PWD
     status --is-command-substitution; and return
-    autojump --add (pwd) >/dev/null 2>>$AUTOJUMP_ERROR_PATH &
+    autojump -a (pwd) >/dev/null &
 end
 
 
@@ -33,22 +21,26 @@ function __aj_err
     echo $argv 1>&2; false
 end
 
+function __aj_not_found
+    __aj_err "autojump: directory '"$argv"' not found"
+    __aj_err "Try `autojump --help` for more information."
+end
+
+
 # default autojump command
 function j
     switch "$argv"
         case '-*' '--*'
             autojump $argv
         case '*'
-            set -l output (autojump $argv)
-            if test -d "$output"
+            set -l new_path (autojump $argv)
+            if test -d "$new_path"
                 set_color red
-                echo $output
+                echo $new_path
                 set_color normal
-                cd $output
+                cd $new_path
             else
-                __aj_err "autojump: directory '"$argv"' not found"
-                __aj_err "\n$output\n"
-                __aj_err "Try `autojump --help` for more information."
+                __aj_not_found $argv
             end
     end
 end
@@ -67,21 +59,18 @@ end
 
 # open autojump results in file browser
 function jo
-    set -l output (autojump $argv)
-    if test -d "$output"
-        __aj_err "autojump: directory '"$argv"' not found"
-        __aj_err "\n$output\n"
-        __aj_err "Try `autojump --help` for more information."
+    if test -z (autojump $argv)
+        __aj_not_found $argv
     else
         switch (sh -c 'echo ${OSTYPE}')
-            case 'linux*'
+            case linux-gnu
                 xdg-open (autojump $argv)
             case 'darwin*'
                 open (autojump $argv)
             case cygwin
                 cygstart "" (cygpath -w -a (pwd))
             case '*'
-                __aj_error "Unknown operating system: '"$OSTYPE"'"
+                __aj_error "Unknown operating system."
         end
         echo end
     end
