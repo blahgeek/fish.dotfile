b73e777d75c0b375e8a45543475e6111f1461cb8
BlahGeek <blahgeek@gmail.com>
2014-12-15 13:15:34 +0800

    global solarize var

--- color.fish
+++ color.fish
@@ -1,42 +1,43 @@
-set -l base03  "--bold black"
-set -l base02  "black"
-set -l base01  "--bold green"
-set -l base00  "--bold yellow"
-set -l base0   "--bold blue"
-set -l base1   "--bold cyan"
-set -l base2   "white"
-set -l base3   "--bold white"
-set -l yellow  "yellow"
-set -l orange  "--bold red"
-set -l red     "red"
-set -l magenta "magenta"
-set -l violet  "--bold magenta"
-set -l blue    "blue"
-set -l cyan    "cyan"
-set -l green   "green"
+set -g solarized_base03  "--bold black"
+set -g solarized_base02  "black"
+set -g solarized_base01  "--bold green"
+set -g solarized_base00  "--bold yellow"
+set -g solarized_base0   "--bold blue"
+set -g solarized_base1   "--bold cyan"
+set -g solarized_base2   "white"
+set -g solarized_base3   "--bold white"
+set -g solarized_yellow  "yellow"
+set -g solarized_orange  "--bold red"
+set -g solarized_red     "red"
+set -g solarized_magenta "magenta"
+set -g solarized_violet  "--bold magenta"
+set -g solarized_blue    "blue"
+set -g solarized_cyan    "cyan"
+set -g solarized_green   "green"
 
 # Used by fish's completion; see
 # http://fishshell.com/docs/2.0/index.html#variables-color
 
-set -g fish_color_normal      $base0
-set -g fish_color_command     $blue
-set -g fish_color_quote       $cyan
-set -g fish_color_redirection $green
-set -g fish_color_end         $base0
-set -g fish_color_error       $red
-set -g fish_color_param       $base0
-set -g fish_color_comment     $base01
-set -g fish_color_match       $cyan
-set -g fish_color_search_match "--background=$base02"
-set -g fish_color_operator    $orange
-set -g fish_color_escape      $cyan
+set -g fish_color_normal      $solarized_base01
+set -g fish_color_command     $solarized_blue
+set -g fish_color_quote       $solarized_cyan
+set -g fish_color_redirection $solarized_green
+set -g fish_color_end         $solarized_base1
+set -g fish_color_error       $solarized_red
+set -g fish_color_param       $solarized_base01
+set -g fish_color_comment     $solarized_base1
+set -g fish_color_match       $solarized_cyan
+set -g fish_color_search_match "--background=$solarized_base2"
+set -g fish_color_operator    $solarized_orange
+set -g fish_color_escape      $solarized_cyan
+set -g fish_color_autosuggestion $solarized_base1
 
 # Used by fish_prompt
 
-set -g fish_color_hostname    $cyan
-set -g fish_color_cwd         $yellow
-set -g fish_color_git         $green
-set -g fish_color_ssh         $red
+set -g fish_color_hostname    $solarized_cyan
+set -g fish_color_cwd         $solarized_yellow
+set -g fish_color_git         $solarized_green
+set -g fish_color_ssh         $solarized_red
 
 if test "$TERM" = "linux"
     echo -en "\e]PB839496" # S_base00
