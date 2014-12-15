f526f62e600f2fa63be7602bcc1575188337016d
BlahGeek <blahgeek@gmail.com>
2014-12-15 13:31:43 +0800

    use term256 color

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -2,14 +2,6 @@ function fish_prompt --description 'Write out the prompt'
 	
 	set -l last_status $status
 
-	set -l prompt_status
-	if test $last_status -ne 0
-		if not set -q __fish_prompt_status
-			set -g __fish_prompt_status (set_color $fish_color_error)
-		end
-		set prompt_status "$__fish_prompt_status [$last_status]"
-	end
-
 	# Just calculate these once, to save a few cycles when displaying the prompt
 	if not set -q __fish_prompt_hostname
 		set -g __fish_prompt_hostname (hostname|cut -d . -f 1)
@@ -20,12 +12,14 @@ function fish_prompt --description 'Write out the prompt'
     end
 	
 	if set -q VIRTUAL_ENV
-		echo -n -s (set_color -b blue white) "(" (basename "$VIRTUAL_ENV") ")" (set_color $fish_color_normal) " "
+		echo -n -s (set_color $fish_color_venv) "(" (basename "$VIRTUAL_ENV") ")" (set_color $fish_color_normal) " "
 	end
 
     echo -n -s (set_color $fish_color_hostname) "$__fish_prompt_hostname" ' '
     echo -n -s (set_color $fish_color_cwd) (prompt_pwd)
     echo -n -s (set_color $fish_color_git) (__fish_git_prompt)
-    echo -n -s "$prompt_status"
+    if test $last_status -ne 0
+        echo -n -s (set_color $fish_color_error) "[$last_status]"
+    end
     echo -n -s (set_color $fish_color_normal) "> "
 end
