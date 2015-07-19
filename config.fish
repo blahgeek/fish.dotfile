fcfbc37619458161c8f7770ef395b7d3418ca1a6
BlahGeek <i@BlahGeek.com>
2015-07-20 00:37:33 +0800

    use abbr instead of alias (fish 2.2.0)

--- config.fish
+++ config.fish
@@ -1,15 +1,15 @@
 if test -f /usr/local/share/autojump/autojump.fish
-    . /usr/local/share/autojump/autojump.fish
+    source /usr/local/share/autojump/autojump.fish
 else
     if test -f /etc/profile.d/autojump.fish
-        . /etc/profile.d/autojump.fish
+        source /etc/profile.d/autojump.fish
     end
 end
-. ~/.config/fish/virtualfish/virtual.fish
-. ~/.config/fish/virtualfish/auto_activation.fish
-. ~/.config/fish/virtualfish/global_requirements.fish
+source ~/.config/fish/virtualfish/virtual.fish
+source ~/.config/fish/virtualfish/auto_activation.fish
+source ~/.config/fish/virtualfish/global_requirements.fish
 
-. ~/.config/fish/color.fish
+source ~/.config/fish/color.fish
 
 set -g __fish_git_prompt_show_informative_status 1
 set -g ___fish_git_prompt_char_cleanstate "✓"
@@ -17,16 +17,22 @@ set -g ___fish_git_prompt_char_dirtystate "+"
 set -g ___fish_git_prompt_char_invalidstate "×"
 set -g ___fish_git_prompt_char_stagedstate "∙"
 
-alias vi="vim"
-alias du="du -h"
-alias df="df -h"
-alias x="dtrx -r -n"
+if test (echo $FISH_VERSION | sed 's/\.//g') -lt 220
+    echo 'Warning: fish version < 2.2.0, using alias instead of abbr'
+    alias abbr=alias
+end
+
+abbr "!"="sudo"
+abbr vi="vim"
+abbr du="du -h"
+abbr df="df -h"
+abbr x="dtrx -r -n"
+abbr mtr="mtr -t"
+abbr ll="ls -alh"
+abbr yoink="open -a Yoink"
 alias net_class="~/Documents/projects/net_class/net_class.py --path ~/Documents/网络学堂 --size_limit 10000000"
 alias mypasswd="python2 /Users/BlahGeek/Documents/projects/mypasswd/mypasswd.py"
 alias pandoc="pandoc --latex-engine=xelatex -V mainfont=SimSun --template=/Users/BlahGeek/.local/share/pandoc/pm-template.latex"
-alias mtr="mtr -t"
-alias ll="ls -alh"
-alias yoink="open -a Yoink"
 
 set -x GOPATH "$HOME/.golang"
 
