615c30156134dc825185d613256fe5bd2c21f6d3
BlahGeek <i@BlahGeek.com>
2017-10-31 17:25:41 +0800

    use true color for terminal

--- color.fish
+++ color.fish
@@ -1,39 +1,39 @@
-set -l green   "green"
-set -l base03  --bold black
-set -l base02  black
-set -l base01  brgreen
-set -l base00  bryellow
-set -l base0   brblue
-set -l base1   brcyan
-set -l base2   white
-set -l base3   --bold white
-set -l yellow  yellow
-set -l orange  brred
-set -l red     red
-set -l magenta magenta
-set -l violet  brmagenta
-set -l blue    blue
-set -l cyan    cyan
-set -l green   green
+# set -l base03  --bold black
+# set -l base02  black
+# set -l base01  brgreen
+# set -l base00  bryellow
+# set -l base0   brblue
+# set -l base1   brcyan
+# set -l base2   white
+# set -l base3   --bold white
+# set -l yellow  yellow
+# set -l orange  brred
+# set -l red     red
+# set -l magenta magenta
+# set -l violet  brmagenta
+# set -l blue    blue
+# set -l cyan    cyan
+# set -l green   green
 
 # set -g solarized_background_highlight (set_color --background=white)
 
-#set -l base03  002b36
-#set -l base02  073642
-#set -l base01  586e75
-#set -l base00  657b83
-#set -l base0   839496
-#set -l base1   93a1a1
-#set -l base2   eee8d5
-#set -l base3   fdf6e3
-#set -l yellow  b58900
-#set -l orange  cb4b16
-#set -l red     dc322f
-#set -l magenta d33682
-#set -l violet  6c71c4
-#set -l blue    268bd2
-#set -l cyan    2aa198
-#set -l green   859900
+# true color
+set -l base03  002b36
+set -l base02  073642
+set -l base01  586e75
+set -l base00  657b83
+set -l base0   839496
+set -l base1   93a1a1
+set -l base2   eee8d5
+set -l base3   fdf6e3
+set -l yellow  b58900
+set -l orange  cb4b16
+set -l red     dc322f
+set -l magenta d33682
+set -l violet  6c71c4
+set -l blue    268bd2
+set -l cyan    2aa198
+set -l green   859900
 
 # Used by fish's completion; see
 # http://fishshell.com/docs/2.0/index.html#variables-color
