106b08da3a48ec15c2a7d393a5d407ba13fd43ea
ZhaoYikai <zhaoyikai@kuaishou.com>
2019-07-22 17:27:14 +0800

    Load fzf keybindings in macos

--- functions/fish_user_key_bindings.fish
+++ functions/fish_user_key_bindings.fish
@@ -1,4 +1,7 @@
 function fish_user_key_bindings
+    if test -e /usr/local/opt/fzf/shell/key-bindings.fish
+        source /usr/local/opt/fzf/shell/key-bindings.fish
+    end
     if functions --query fzf_key_bindings > /dev/null
         fzf_key_bindings
     end
