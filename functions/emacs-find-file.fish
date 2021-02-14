c0dac8e2acd97e6af8aed56dc92b0652b2a91bb5
BlahGeek <i@blahgeek.com>
2021-02-14 20:16:11 +0800

    Fix find-file in remote

--- functions/emacs-find-file.fish
+++ functions/emacs-find-file.fish
@@ -5,6 +5,13 @@ function emacs-find-file -d "Find file inside emacs"
         set filename (realpath $filename)
     end
 
+    if set -q SSH_CONNECTION
+        set -l ssh_connection_arr (string split ' ' $SSH_CONNECTION)
+        set -l ssh_host "$ssh_connection_arr[3]"
+        set -l ssh_port "$ssh_connection_arr[4]"
+        set filename "/ssh:"$USER"@"$ssh_host"#"$ssh_port":"$filename
+    end
+
     echo "Finding file $filename"
     emacs_vterm_cmd find-file "$filename"
 end
