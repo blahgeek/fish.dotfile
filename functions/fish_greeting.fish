9810763d3cc10fa9e50880e2a10dd715de830514
BlahGeek <i@blahgeek.com>
2021-07-08 23:36:39 +0800

    Remove `^/dev/null` (invalid in fish 3.3)

--- functions/fish_greeting.fish
+++ functions/fish_greeting.fish
@@ -1,27 +1,27 @@
 function fish_greeting --description 'Fish greeting~'
-    if type archey3 > /dev/null ^/dev/null
+    if type archey3 > /dev/null 2> /dev/null
         archey3
         return
     end
-    if type archey > /dev/null ^/dev/null
+    if type archey > /dev/null 2> /dev/null
         archey
         return
     end
     echo
-    if type uname > /dev/null ^/dev/null
+    if type uname > /dev/null 2> /dev/null
         echo "Machine:  " (uname -prs)
     end
-    if type date > /dev/null ^/dev/null
+    if type date > /dev/null 2> /dev/null
         echo "Date:     " (date)
     end
-    if type uptime > /dev/null ^/dev/null
+    if type uptime > /dev/null 2> /dev/null
         echo "Uptime:   " (uptime)
     end
-    if type df > /dev/null ^/dev/null
+    if type df > /dev/null 2> /dev/null
         echo
-        df -hl | grep --color=never "^/dev/" | grep -v "/snap"
+        df -hl | grep --color=never "2> /dev/" | grep -v "/snap"
     end
-    if type fortune >/dev/null ^/dev/null
+    if type fortune >/dev/null 2> /dev/null
         echo
         fortune -a -s
         echo
